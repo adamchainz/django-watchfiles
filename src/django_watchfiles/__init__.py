4e22345664d1387771695bafd9374dc754ccb755
Nathan <shark404@gmail.com>
2024-01-24 20:46:43 +1000

    Fix Python 3.8 compatibility with Path.relative_to() (#68)
    Co-authored-by: Adam Johnson <me@adamj.eu>

--- src/django_watchfiles/__init__.py
+++ src/django_watchfiles/__init__.py
@@ -62,10 +62,14 @@ class WatchfilesReloader(autoreload.BaseReloader):
             # print("Path in watched files")
             return True
         for directory, globs in self.directory_globs.items():
-            if path.is_relative_to(directory):
+            try:
+                relative_path = path.relative_to(directory)
+            except ValueError:
+                pass
+            else:
                 # print("Path is sub dir")
                 for glob in globs:
-                    if fnmatch.fnmatch(str(path.relative_to(directory)), glob):
+                    if fnmatch.fnmatch(str(relative_path), glob):
                         # print("Path is glob match")
                         return True
         # print("file filter", change, path)

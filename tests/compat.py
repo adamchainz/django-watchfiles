f701f78d87f0e9bbf9331f61cc5cb34f85e757d4
Adam Johnson <me@adamj.eu>
2024-10-28 15:24:37 +0000

    Add some proper tests (#122)

---
+++ tests/compat.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import sys
+from contextlib import AbstractContextManager
+from typing import Any
+from typing import Callable
+from typing import TypeVar
+
+from django import test
+
+_T = TypeVar("_T")
+
+if sys.version_info < (3, 11):
+
+    def _enter_context(cm: Any, addcleanup: Callable[..., None]) -> Any:
+        # We look up the special methods on the type to match the with
+        # statement.
+        cls = type(cm)
+        try:
+            enter = cls.__enter__
+            exit = cls.__exit__
+        except AttributeError:  # pragma: no cover
+            raise TypeError(
+                f"'{cls.__module__}.{cls.__qualname__}' object does "
+                f"not support the context manager protocol"
+            ) from None
+        result = enter(cm)
+        addcleanup(exit, cm, None, None, None)
+        return result
+
+
+class SimpleTestCase(test.SimpleTestCase):
+    if sys.version_info < (3, 11):
+
+        def enterContext(self, cm: AbstractContextManager[_T]) -> _T:
+            result: _T = _enter_context(cm, self.addCleanup)
+            return result
